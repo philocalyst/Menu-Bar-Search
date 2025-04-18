eff5c231fd7efffe774a4bc6b0812a7217b03aff
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-18 14:36:25 -0400

    Added argument parser
    All depedencies at latest version

--- menu/Package.swift
+++ menu/Package.swift
@@ -7,14 +7,19 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-protobuf.git",
-            from: "1.6.0"
-        )
+            from: "1.29.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "1.5.0"
+        ),
     ],
     targets: [
         .executableTarget(
             name: "menu",
             dependencies: [
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             linkerSettings: [
                 .unsafeFlags([
