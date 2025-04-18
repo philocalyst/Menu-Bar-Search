274a57b13f11fbe671bbc78157f24543b716f26c
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-18 13:11:55 -0400

    Modernized the package.swift
    - Using swift tools 6.1
    - Latest protobuf
    - Removed help comments

--- menu/Package.swift
+++ menu/Package.swift
@@ -1,31 +1,32 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:6.1
 import PackageDescription
 
 let package = Package(
     name: "menu",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(
+            url: "https://github.com/apple/swift-protobuf.git",
+            from: "1.6.0"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "menu",
-            dependencies: ["SwiftProtobuf"],
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ],
             linkerSettings: [
                 .unsafeFlags([
                     "-Xlinker", "-sectcreate",
                     "-Xlinker", "__TEXT",
                     "-Xlinker", "__info_plist",
-                    "-Xlinker", "Sources/Info.plist"
+                    "-Xlinker", "Sources/Info.plist",
                 ])
-            ]),
+            ]
+        ),
         .testTarget(
             name: "menuTests",
-            dependencies: ["menu"]),
+            dependencies: ["menu"]
+        ),
     ]
 )
